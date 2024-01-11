0d7e808635c5b9d7052e99003a74dd76c3fb5468
Andrea Vigani <and.vigani@gmail.com>
2024-01-11 16:04:04 +0100

    autocomplete beta

--- components/composite/StepCustomer/BillingAddressFormNew/index.tsx
+++ components/composite/StepCustomer/BillingAddressFormNew/index.tsx
@@ -1,5 +1,6 @@
 import { Address } from "@commercelayer/sdk"
-import { useContext } from "react"
+import { Loader } from "@googlemaps/js-api-loader"
+import { useContext, useState } from "react"
 import styled from "styled-components"
 import tw from "twin.macro"
 
@@ -21,6 +22,93 @@ export const BillingAddressFormNew: React.FC<Props> = ({
   if (!appCtx) {
     return null
   }
+
+  const [autocompleteAddress, setAutocompleteAddress] = useState({
+    line_1: billingAddress?.line_1 || "",
+    line_2: billingAddress?.line_2 || "",
+    city: billingAddress?.city || "",
+    country_code: billingAddress?.country_code || "",
+    state_code: billingAddress?.state_code || "",
+    zip_code: billingAddress?.zip_code || "",
+    // line_1: "",
+    // line_2: "",
+    // city: "",
+    // country_code: "",
+    // state_code: "",
+    // zip_code: "",
+  })
+
+  const loader = new Loader({
+    apiKey: "AIzaSyCGyK40kmxyQsrl_QcwrvptKj5bb5J1Q6U",
+    libraries: ["places"],
+  })
+  loader.load().then(() => {
+    const input = document.getElementById("billing_address_line_1")
+
+    if (input) {
+      const autocomplete = new google.maps.places.Autocomplete(input)
+
+      autocomplete.addListener("place_changed", () => {
+        const selectedPlace = autocomplete.getPlace()
+        const addressComponents = selectedPlace.address_components || []
+
+        if (selectedPlace) {
+          const newAutocompleteAddress: Partial<typeof autocompleteAddress> = {}
+
+          const countryComponent = addressComponents.find((component) =>
+            component.types.includes("country")
+          )
+          const selectedCountryCode = countryComponent?.short_name || ""
+
+          addressComponents.forEach((component) => {
+            switch (component.types[0]) {
+              case "street_number":
+                newAutocompleteAddress.line_1 =
+                  (newAutocompleteAddress.line_1 || "") +
+                  component.long_name +
+                  " "
+                break
+              case "route":
+                newAutocompleteAddress.line_1 =
+                  (newAutocompleteAddress.line_1 || "") + component.long_name
+                break
+              case "locality":
+                newAutocompleteAddress.city = component.long_name
+                break
+              case "country":
+                newAutocompleteAddress.country_code = component.short_name
+                break
+              case "administrative_area_level_1":
+                if (selectedCountryCode !== "IT") {
+                  newAutocompleteAddress.state_code = component.short_name
+                }
+                break
+              case "administrative_area_level_2":
+                if (selectedCountryCode === "IT") {
+                  newAutocompleteAddress.state_code = component.short_name
+                }
+                break
+              case "postal_code":
+                newAutocompleteAddress.zip_code = component.long_name
+                break
+              default:
+                break
+            }
+          })
+
+          setAutocompleteAddress({
+            ...autocompleteAddress,
+            ...newAutocompleteAddress,
+          })
+
+          console.log(selectedPlace)
+          console.log(newAutocompleteAddress)
+          // console.log("billingAddress", billingAddress)
+          // console.log("autocompleteAddress", autocompleteAddress)
+        }
+      })
+    }
+  })
 
   const { requiresBillingInfo } = appCtx
 
@@ -63,28 +151,28 @@ export const BillingAddressFormNew: React.FC<Props> = ({
         fieldName="billing_address_line_1"
         resource="billing_address"
         type="text"
-        value={billingAddress?.line_1 || ""}
+        value={autocompleteAddress?.line_1 || ""}
       />
       <AddressInputGroup
         fieldName="billing_address_line_2"
         resource="billing_address"
         required={false}
         type="text"
-        value={billingAddress?.line_2 || ""}
+        value={autocompleteAddress?.line_2 || ""}
       />
       <Grid>
         <AddressInputGroup
           fieldName="billing_address_city"
           resource="billing_address"
           type="text"
-          value={billingAddress?.city || ""}
+          value={autocompleteAddress?.city || ""}
         />
         <AddressInputGroup
           fieldName="billing_address_country_code"
           resource="billing_address"
           type="text"
           openShippingAddress={openShippingAddress}
-          value={billingAddress?.country_code || ""}
+          value={autocompleteAddress?.country_code || ""}
         />
       </Grid>
       <Grid>
@@ -92,13 +180,13 @@ export const BillingAddressFormNew: React.FC<Props> = ({
           fieldName="billing_address_state_code"
           resource="billing_address"
           type="text"
-          value={billingAddress?.state_code || ""}
+          value={autocompleteAddress?.state_code || ""}
         />
         <AddressInputGroup
           fieldName="billing_address_zip_code"
           resource="billing_address"
           type="text"
-          value={billingAddress?.zip_code || ""}
+          value={autocompleteAddress?.zip_code || ""}
         />
       </Grid>
       <AddressInputGroup
